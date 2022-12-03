8ccf006db9348c470166c8a38a223c1ab8543062
Douglas Starnes <douglas.a.starnes@gmail.com>
2022-12-03 11:46:07 -0600

    Update main.py

--- day03/main.py
+++ day03/main.py
@@ -3,13 +3,13 @@ from string import ascii_letters
 with open("./day03/ input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
-
+# I'm not using this function, there is an easier way to get the priority of the items
 def get_priority(item):
     if item.islower():
         return ord(item) - ord("a") + 1
     return ord(item) - ord("A") + 27
 
-
+# Part One
 total = 0
 for line in lines:
     half = len(line) // 2
@@ -18,6 +18,7 @@ for line in lines:
     total += ascii_letters.index(common.pop()) + 1
 print(total)
 
+# Part Two
 total = 0
 for x in range(0, len(lines) - 1, 3):
     group = lines[x:x+3]
